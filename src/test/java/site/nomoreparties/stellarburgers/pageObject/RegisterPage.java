05946700821cae41d56f3187d8b45e3761f2c0ee
Lora Myasnikova <loramya@yandex.ru>
2022-05-13 23:48:59 +0700

    Add RegisterPage.java

---
+++ src/test/java/site/nomoreparties/stellarburgers/pageObject/RegisterPage.java
@@ -0,0 +1,48 @@
+package site.nomoreparties.stellarburgers.pageObject;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class RegisterPage {
+
+    final static public String URL = "https://stellarburgers.nomoreparties.site/register";
+
+    @FindBy(how = How.XPATH, using = "./form/fieldset[1]/div/div/input")
+    private SelenideElement nameField;
+
+    @FindBy(how = How.XPATH, using = ".//form/fieldset[2]/div/div/input")
+    private SelenideElement emailField;
+
+    @FindBy(how = How.XPATH, using = ".//form/fieldset[3]/div/div/input")
+    private SelenideElement passwordField;
+
+    @FindBy(how = How.XPATH, using = ".//form/button")
+    private SelenideElement registerButton;
+
+    @FindBy(how = How.XPATH, using = ".//div/div/p/a")
+    private SelenideElement loginLink;
+
+    public void setNameField(String name) {
+        nameField.setValue(name);
+    }
+
+    public void setEmailField(String email) {
+        emailField.setValue(email);
+    }
+
+    public void setPasswordField(String password) {
+        passwordField.setValue(password);
+    }
+
+    public void clickRegisterButton() {
+        registerButton.click();
+    }
+
+    public void setRegistrationForm(String name, String email, String password) {
+        setNameField(name);
+        setEmailField(email);
+        setPasswordField(password);
+    }
+
+}
