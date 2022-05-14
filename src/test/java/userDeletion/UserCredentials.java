42aa97034142fdf01831d6995bfcc8d3570f1a91
Lora Myasnikova <loramya@yandex.ru>
2022-05-14 17:57:34 +0700

    Add api for deletion users after tests

---
+++ src/test/java/userDeletion/UserCredentials.java
@@ -0,0 +1,13 @@
+package userDeletion;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserCredentials {
+
+    private String email;
+    private String password;
+
+}
