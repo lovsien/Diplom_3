e793e66eab37be63e599c3073cc0737be14370c1
Lora Myasnikova <loramya@yandex.ru>
2022-05-14 17:56:23 +0700

    Refine tests

--- src/test/java/site/nomoreparties/stellarburgers/RegistrationTest.java
+++ src/test/java/site/nomoreparties/stellarburgers/RegistrationTest.java
@@ -1,8 +1,5 @@
 package site.nomoreparties.stellarburgers;
 
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
-import com.sun.xml.bind.v2.TODO;
 import io.restassured.response.ValidatableResponse;
 import org.junit.After;
 import org.junit.Test;
@@ -12,11 +9,13 @@ import userDeletion.UserClient;
 import userDeletion.UserCredentials;
 
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.webdriver;
+import static com.codeborne.selenide.WebDriverConditions.url;
 
 public class RegistrationTest {
 
     final String name = "Вася";
-    final String email = "vasya@mail.com";
+    final String email = "Asdf@mail.com";
     final String correctPassword = "12345678";
     final String password5digits = "12345";
     final String password6digits = "123456";
@@ -35,22 +34,22 @@ public class RegistrationTest {
         }
     }
 
-    //TODO: прописать проверку на URL после регистрации
     @Test
     public void userCanRegisterWithCorrectData() {
         RegisterPage registerPage = open(RegisterPage.URL, RegisterPage.class);
 
         registerPage.setRegistrationForm(name, email, correctPassword);
         registerPage.checkIncorrectPasswordMessageDoesNotAppear();
-        registerPage.clickRegisterButton();
 
-        String currentURL = WebDriverRunner.url();
-        currentURL.equals(LoginPage.URL);
+        registerPage.clickRegisterButton();
+        registerPage.checkExistingUserMessageDoesNotAppear();
+        webdriver().shouldHave(url(LoginPage.URL));
     }
 
     @Test
     public void errorMessageAppearWhenPassword5Digits() {
         RegisterPage registerPage = open(RegisterPage.URL, RegisterPage.class);
+
         registerPage.setRegistrationForm(name, email, password5digits);
         registerPage.clickRegisterButton();
         registerPage.checkIncorrectPasswordErrorMessage();
@@ -59,9 +58,9 @@ public class RegistrationTest {
     @Test
     public void errorMessageDoesNotAppearWhenPassword6Digits() {
         RegisterPage registerPage = open(RegisterPage.URL, RegisterPage.class);
+
         registerPage.setRegistrationForm(name, email, password6digits);
         registerPage.checkIncorrectPasswordMessageDoesNotAppear();
-        registerPage.clickRegisterButton();
     }
 
 }
