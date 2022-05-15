a3e7a5b8db6620ebca1acc9ed67d6bc971e0dc86
Lora Myasnikova <loramya@yandex.ru>
2022-05-15 17:01:19 +0700

    Add Step and checkButtonHasHrefToAccount

--- src/test/java/site/nomoreparties/stellarburgers/pageObject/HeaderPage.java
+++ src/test/java/site/nomoreparties/stellarburgers/pageObject/HeaderPage.java
@@ -1,21 +1,31 @@
 package site.nomoreparties.stellarburgers.pageObject;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class HeaderPage {
 
-    @FindBy(how = How.XPATH, using = "/html/body/div[1]/div/header/nav/div/a")
+    @FindBy(how = How.XPATH, using = ".//header/nav/div/a")
     private SelenideElement logo;
 
-    @FindBy(how = How.XPATH, using = "/html/body/div[1]/div/header/nav/a")
+    @FindBy(how = How.XPATH, using = ".//header/nav/a")
     private SelenideElement personalAccountButton;
 
-    @FindBy(how = How.XPATH, using = "/html/body/div[1]/div/header/nav/ul/li[2]/a")
+    @FindBy(how = How.XPATH, using = ".//header/nav/ul/li[2]/a")
     private SelenideElement ordersQueueButton;
 
-    @FindBy(how = How.XPATH, using = "/html/body/div[1]/div/header/nav/ul/li[1]/a")
+    @FindBy(how = How.XPATH, using = ".//header/nav/ul/li[1]/a")
     private SelenideElement constructorButton;
 
+    @Step("Click on personal account button in header")
+    public void clickPersonalAccountButton() {
+        personalAccountButton.click();
+    }
+
+    public void checkButtonHasHrefToAccount() {
+        personalAccountButton.shouldHave(Condition.href("/account"));
+    }
 }
