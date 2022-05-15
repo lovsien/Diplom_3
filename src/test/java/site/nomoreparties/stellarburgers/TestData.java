6d5f22a933aac98ba34ecfa909f39d8c3184d3f4
Lora Myasnikova <loramya@yandex.ru>
2022-05-15 16:54:24 +0700

    Add TestData.java for random test data

---
+++ src/test/java/site/nomoreparties/stellarburgers/TestData.java
@@ -0,0 +1,23 @@
+package site.nomoreparties.stellarburgers;
+
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
+import org.apache.commons.lang3.RandomStringUtils;
+import site.nomoreparties.stellarburgers.userDeletion.User;
+
+public class TestData {
+
+    @Step("Create random data for test")
+    public User getRandomUserTestData() {
+        final String name = RandomStringUtils.randomAlphabetic(4);
+        final String email = RandomStringUtils.randomAlphabetic(4).toLowerCase() + "@mail.com";
+        final String password = RandomStringUtils.randomAlphabetic(8);
+
+        Allure.addAttachment("Name: ", name);
+        Allure.addAttachment("Email: ", email);
+        Allure.addAttachment("Password: ", password);
+
+        return User.builder().name(name).email(email).password(password).build();
+    }
+
+}
